80ad4d8095e50a3063746f73b12bbd3e7588775a
aakashkr <aspakash000@gmail.com>
2024-04-04 15:37:31 +0530

    navbar created

---
+++ components/Sidebar.tsx
@@ -0,0 +1,27 @@
+// "use client"
+
+import MoreDropdown from "./MoreDropdown";
+import NavLinks from "./NavLinks";
+
+// import { useState } from "react";
+export default function Sidebar () {
+  //   const [isOpen, setIsOpen] = useState(false);
+
+  // const toggleDropdown = () => {
+  //   setIsOpen(!isOpen);
+  
+  return (
+    <>
+    <div className="flex w-64 h-screen fixed flex-col px-3 py-4 md:px-2 border-r-2">
+      <div className="border-t -ml-3 md:ml-0 bg-white dark:bg-neutral-950 h-16 justify-evenly fixed z-50 flex-1 w-full md:relative md:h-full bottom-0 md:border-none flex flex-row md:justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 p-2">
+        <h1>Network</h1>
+        <NavLinks />
+
+        <div className="hidden md:flex relative md:mt-auto flex-1 items-end w-full">
+          <MoreDropdown />
+        </div>
+      </div>
+    </div>
+    </>
+  );
+};
